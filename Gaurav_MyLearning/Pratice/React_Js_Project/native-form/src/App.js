98c7528a9f6adddb4ccf1ae2d08f4eb9d95d2717
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-09-06 12:14:32 +0530

    native form completed

--- Gaurav_MyLearning/Pratice/React_Js_Project/native-form/src/App.js
+++ Gaurav_MyLearning/Pratice/React_Js_Project/native-form/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const initialValue = { username: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValue);
   const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
   // console.log(formValues);
   // let count= 0;
   function handleChange(e) {
@@ -22,7 +23,13 @@ function App() {
     console.log("Submit==>", formValues);
     //Validate the formValues
     setFormErrors(validate(formValues));
+    setIsSubmit(true);
   }
+  useEffect(() => {
+    if(Object.keys(formErrors).length===0 && isSubmit){
+      console.log(formValues);
+    }
+  }, [formErrors]);
 
   function validate(values) {
     const errors = {};
@@ -34,7 +41,23 @@ function App() {
       errors.username = "Invalid username";
       console.log(errors.username);
     }
-    if (values.email && !regex_email.test(values.email)) {
+    // const check_value=(!regex_email.test(value.email));
+    // console.log("return of !regex_email.test(value.email) =",check_value );
+    // const check_value2=values.email;
+    if(!regex_email.test(values.email))
+    {
+      console.log("if working for !regex_email.test(value.email)");
+    }
+    if(values.email)
+    {
+      console.log("if working for values.email")
+    }
+
+    if (values.email==="") {
+      errors.email = "Enter email Id";
+      console.log(errors.email);
+    }
+     else if (!regex_email.test(values.email)) {
       errors.email = "Invalid Email ID";
       console.log(errors.email);
     }
@@ -63,7 +86,19 @@ function App() {
 
   return (
     <div className='container'>
-      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      {
+        Object.keys(formErrors).length ===0 && isSubmit ?
+        (<div className='ui message success'>Signed is Successfully</div>) :
+        (
+        <div>
+           <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+           {/* <div className='ui message unsuccess'>Signed is UnSuccessfully</div> */}
+           <div className='ui message unsuccess' style={{color:"red", background:"yellow",}}>Signed is UnSuccessfully</div>
+           {/* <div >Signed is UnSuccessfully</div> */}
+        </div>
+         
+        )
+      }
       <form onSubmit={handleSubmit}>
         <h1>Login form</h1>
         <div className='ui divider'></div>
