f1ca6007e9dd3f341f01cbff420eacb29bbc7807
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-09-18 13:10:53 +0530

    usemeno done

---
+++ Gaurav_MyLearning/Pratice/React_Js_Project/Hooks_Understanding/use-memo2/src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, {useState, useMemo } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(1);
+  const [name, setName] = useState("");
+  let result = useMemo(() => {
+    return(factorial(counter));
+  }, [counter]);
+
+  return (
+    <div className='App'>
+      <div >
+        <h1>Factorial of {counter} is <span>{result}</span></h1>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      </div>
+      <hr />
+      <div>
+        <label>Name</label>
+        <input type="text" name="name" placeholder="Enter your name" onChange={(e)=> setName(e.target.value)}></input>
+        <hr/>
+        <DisplayName name = {name} />
+        {/* <p>{`My name is ${name}`}</p> */}
+
+      </div>
+
+    </div>
+
+
+
+  );
+}
+function factorial(n) {
+  let i=0;
+  while(i < 200000000)
+  i++;
+  if (n < 0)
+    return -1;
+  if (n === 0)
+    return 1;
+
+  return n * factorial(n - 1);
+}
+const DisplayName = React.memo(({name}) => {
+  console.log("name =>", name);
+  return <p>{`My b=name is ${name}`}</p>
+})
+
+export default App;
