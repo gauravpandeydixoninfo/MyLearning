2846c4511500b4779c0f92fea5ffd8ef0b84e623
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-10-06 17:34:09 +0530

    working on mongodb

--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/Backend_tutorial/mongodb/index.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/Backend_tutorial/mongodb/index.js
@@ -1,6 +1,6 @@
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://192.168.56.112/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 var db=mongoose.connection;
@@ -9,19 +9,19 @@ db.once('open', function(){
     console.log("we are connected with mongodb database friend");
 });
 // creating a type of template
-const kittySchema = new mongoose.Schema({
-    name: String
-  });
+// const kittySchema = new mongoose.Schema({
+//     name: String
+//   });
 
-var harrykitten= new Kitten({ name: 'harryKitty' });
-console.log(harrykitty.name); // 'Silence'
+// var harrykitten= new Kitten({ name: 'harryKitty' });
+// console.log(harrykitty.name); // 'Silence'
 
-kittySchema.methods.speak = function () {
-    var greeting = this.name
-    console.log(greeting);
-  }
+// kittySchema.methods.speak = function () {
+//     var greeting = this.name
+//     console.log(greeting);
+//   }
 
-harrykitty.save(function (err, harrykitty) {
-    if (err) return console.error(err);
-    harrykitty.speak();
-  });
+// harrykitty.save(function (err, harrykitty) {
+//     if (err) return console.error(err);
+//     harrykitty.speak();
+//   });
