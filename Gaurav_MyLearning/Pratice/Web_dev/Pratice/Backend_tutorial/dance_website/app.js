20d2ec714a8875e94a713df43d02cddd1d1b7bfe
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-10-09 14:31:10 +0530

    mongodb full project

--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/Backend_tutorial/dance_website/app.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/Backend_tutorial/dance_website/app.js
@@ -2,6 +2,19 @@ const express=require("express");
 const path=require("path");
 const fs=require("fs");
 const app=express();
+const bodyparser=require("body-parser");
+var mongoose=require("mongoose");
+mongoose.coonect('mongodb://localhost/contactDance', {useNewParser: true})
+var contactSchema = new moongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    desc: String,
+    name: String,
+});
+
+var Contact = mongoose.model('Contact', contactSchema);
+
 const port=3000;
 //express specific stuff
 app.use(express.urlencoded());
@@ -14,7 +27,24 @@ app.set('views', path.join(__dirname, 'views'));  //set the views directory
 //Endpoints
 app.get('/',(req,res)=>{
     const parms={};
-    res.status(200).render('index.pug', parms);
+    res.status(200).render('home.pug', parms);
+})
+
+app.get('/contact',(req,res)=>{
+    const parms={};
+    res.status(200).render('contact.pug', parms);
+})
+
+app.post('/contact',(req,res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This iteam has been saved successfully");
+    }).catch(()=>{
+        res.status(400).send("Iteam was not saved to the database");
+    });
+    })
+    const parms={};
+    res.status(200).render('contact.pug', parms);
 })
 
 app.listen(port,()=>{
