7ddda10e2f2c918a54a570cac24d1a69ad28ce21
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-10-31 12:35:30 +0530

    infinite scrolling working fine

--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/news_proj/src/App.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/news_proj/src/App.js
@@ -6,22 +6,35 @@ import React, { Component } from 'react';
 import News from './components/News.js';
 import NavBar from './components/NavBar.js';
 import { BrowserRouter as Router, Switch,  Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
-  
+  state= {
+    progress: 0
+  }
+  setProgress=(progress) =>{
+    this.setState({progress: progress})
+  }
+  // apiKey=process.env.REACT_APP_NEWS_API;
+  apiKey='5a0e740334694b1ea36ba2a1b410c030';
   render() {
     return (
       <div>
         <NavBar/>
+        <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+        />
         <Router>
           <Switch>
-        <Route exact path='/'><News key="general" pageSize={5} category="general" country="in"/></Route>
-        <Route exact path='/Business'><News key="business" pageSize={5} category="business" country="in"/></Route>
-        <Route exact path='/Entertainment'><News key="entertainment" pageSize={5} category="entertainment" country="in"/></Route>
-        <Route exact path='/Health'><News key="health" pageSize={5} category="health" country="in"/></Route>
-        <Route exact path='/Science'><News key="science" pageSize={5} category="science" country="in"/></Route>
-        <Route exact path='/Sports'><News key="sports" pageSize={5} category="sports" country="in"/></Route>
-        <Route exact path='/Technology'><News key="technology" pageSize={5} category="technology" country="in"/></Route>
+        <Route exact path='/'><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={20} category="general" country="in"/></Route>
+        <Route exact path='/Business'><News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={20} category="business" country="in"/></Route>
+        <Route exact path='/Entertainment'><News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={20} category="entertainment" country="in"/></Route>
+        <Route exact path='/Health'><News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={20} category="health" country="in"/></Route>
+        <Route exact path='/Science'><News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={20} category="science" country="in"/></Route>
+        <Route exact path='/Sports'><News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={20} category="sports" country="in"/></Route>
+        <Route exact path='/Technology'><News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={20} category="technology" country="in"/></Route>
         </Switch>
         </Router>
       </div>
--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/news_proj/src/components/News.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/news_proj/src/components/News.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import NewsIteam from './NewsIteam';
 import Spinner from './Spinner.js';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 
 export default class News extends Component {
     articles = [{
         "source": {
-          "id": null,
-          "name": "MacRumors"
+            "id": null,
+            "name": "MacRumors"
         },
         "author": "Tim Hardwick",
         "title": "2024 iPad Pro to Use LCD Panel Instead of OLED or Mini-LED, Claims Sketchy Report",
@@ -17,11 +19,11 @@ export default class News extends Component {
         "urlToImage": "https://images.macrumors.com/t/Y64VSIYOTQARD3TFVrlKnxIGRe4=/2500x/article-new/2023/08/ipad-pro-red.jpg",
         "publishedAt": "2023-10-20T09:03:02Z",
         "content": "Apple's next-generation 12.9-inch iPad Pro will revert to an LCD display instead of adopting OLED panel technology, claims a controversial new report coming out of Asia.\r\nIt has been widely reported … [+2575 chars]"
-      },
-      {
+    },
+    {
         "source": {
-          "id": null,
-          "name": "NPR"
+            "id": null,
+            "name": "NPR"
         },
         "author": "Mara Hoplamazian",
         "title": "A bad apple season has some U.S. fruit growers planning for life in a warmer world",
@@ -30,11 +32,11 @@ export default class News extends Component {
         "urlToImage": "https://media.npr.org/assets/img/2023/10/19/img_3466_wide-9b00042b669a75ebf086af5d9f522640a3c50bef-s1400-c100.jpg",
         "publishedAt": "2023-10-20T09:30:29Z",
         "content": "Chuck and Diane Southers' apple orchard stretches over about 30 hilly acres in Concord, New Hampshire. A hard freeze in May killed most of their apple crop.\r\nMara Hoplamazian/NHPR\r\nCONCORD, N.H. Chuc… [+5438 chars]"
-      },
-      {
+    },
+    {
         "source": {
-          "id": null,
-          "name": "Xataka.com"
+            "id": null,
+            "name": "Xataka.com"
         },
         "author": "Roberto Méndez",
         "title": "Las mejores ofertas de la semana que no deberías dejar escapar en smart TVs, teléfonos, portátiles, consolas y más: Cazando Gangas",
@@ -43,11 +45,11 @@ export default class News extends Component {
         "urlToImage": "https://i.blogs.es/3ccf4b/cazando-gangas-octubre-20/840_560.jpeg",
         "publishedAt": "2023-10-20T08:40:23Z",
         "content": "Con la llegada de un nuevo viernes, es el momento perfecto para repasar algunas de las ofertas más tentadoras para este fin de semana, las cuales nos ayudarán a llenar nuestros bolsillos de ahorros. … [+10067 chars]"
-      },
-      {
+    },
+    {
         "source": {
-          "id": null,
-          "name": "Xataka.com"
+            "id": null,
+            "name": "Xataka.com"
         },
         "author": "Enrique Pérez",
         "title": "Amazon y X están tranquilas frente a la normativa de Europa. No les tocará pagar nada",
@@ -56,82 +58,124 @@ export default class News extends Component {
         "urlToImage": "https://i.blogs.es/0798bc/musk-europa-jassy/840_560.jpeg",
         "publishedAt": "2023-10-20T07:57:59Z",
         "content": "La Unión Europea ya ha empezado a aplicar la nueva Ley de Servicios Digitales (DSA) y una empresa como X ya se ha convertido en firme candidata a ser la primera en ser multada, por su falta de medida… [+2260 chars]"
-      }
-    
-      ]
+    }
+
+    ]
     constructor() {
-      super();
-      console.log("Hellow I am constructor");
-      this.state = {
-        articles: this.articles,
-        loading: false,
-        page: 1,
-        totalResults:this.totalResults 
-      }
-      console.log("value to this.state.totalResults==>"+ this.state.totalResults);
+        super();
+        console.log("Hellow I am constructor");
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1,
+            totalResults: 0
+        }
+        console.log("value to this.state.totalResults==>" + this.state.totalResults);
     }
     componentDidMount() {
-      fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5a0e740334694b1ea36ba2a1b410c030&pageSize=${this.props.pageSize}`)
-      
-        .then(res => res.json())
-        .then(this.setState({loading: true}))
-        .then(json => this.setState({ articles: json.articles, totalResults: json.totalResults, loading: false }));
-        console.log("totalResult in componentdidMount="+ this.articles.totalResults)
-  
+        // fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5a0e740334694b1ea36ba2a1b410c030&pageSize=${this.props.pageSize}`)
+
+        //     .then(res => res.json())
+        //     .then(this.setState({ loading: true }))
+        //     .then(json => this.setState({ articles: json.articles, totalResults: json.totalResults, loading: false }));
+        // console.log("totalResult in componentdidMount=" + this.articles.totalResults )
+        // console.log("totalResult in componentdidMount=" + this.state.totalResults )
+        // console.log("this.props.category ==>" +this.props.category)
+        this.tooglePage();
+
     }
-    tooglePage=async ()=>{
-      fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5a0e740334694b1ea36ba2a1b410c030&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`)
-        .then(res => res.json())
-         .then(this.setState({loading:true}))
-        .then(json => this.setState({
-          articles: json.articles,
-          loading: false
-         }));
-  
-  
+    tooglePage = async () => {
+        this.props.setProgress(10);
+        this.setState({page: this.state.page +1});
+        fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page +1}&pageSize=${this.props.pageSize}`)
+            .then(res => res.json())
+            
+            .then(this.setState({ loading: true }))
+        
+            .then(json => this.setState({
+                articles: json.articles,
+                loading: false,
+                totalResults: json.totalResults
+            }));
+            this.props.setProgress(100);
+            console.log(this.state.totalResults);
+
+
     }
-  
-    handleNextClick= async ()=>{
-  
-      if(!(this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)))
-    {await this.setState({
-        page: this.state.page + 1,
-      })
-      this.tooglePage();}
+    fetchMoreData = async() =>{
+         this.setState({page: this.state.page +1});
+        fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`)
+            .then(res => res.json())
+            .then(this.setState({ loading: true }))
+            .then(json => this.setState({
+                articles: this.state.articles.concat(json.articles),
+                loading: false,
+                totalResults: json.totalResults
+            }));
+        
     }
-     handlePrevClick=async ()=>{
-  
-      await this.setState({
-        page: this.state.page - 1,
-      })
-      this.tooglePage();
+
+    handleNextClick = async () => {
+
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+            await this.setState({
+                page: this.state.page + 1,
+            })
+            this.tooglePage();
+        }
+    }
+    handlePrevClick = async () => {
+
+        await this.setState({
+            page: this.state.page - 1,
+        })
+        this.tooglePage();
     }
     render() {
-      return (
-        <div className='container my-3'>
-          <h2 className='text-center'> GauravNews - Top Headliens</h2>
-          {this.state.loading&&<Spinner/>}
-          <div className="row">
-            {this.state.articles.map((element) => {
-              return <div className="col-md-4">
-                <NewsIteam title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-              </div>
-            })}
-          </div>
-          <div className="container d-flex justify-content-between">
+        return (
+            <div className='container my-3'>
+                <h2 className='text-center'> GauravNews - Top Headliens</h2>
+                {/* {this.state.loading&&<Spinner/>} */}
+                <InfiniteScroll
+                  
+                   dataLength={this.state.articles.length} //This is important field to render the next data
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length!==this.state.totalResults}
+                    loader={<Spinner/>}>
+{/* <div className="container d-flex justify-content-between">
             <button disabled={this.state.page<=1} type="button" className="btn btn-dark left" onClick={this.handlePrevClick}>&larr; Left</button>
             <button disable={this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark right" onClick={this.handleNextClick}>&rarr; Right</button>
         
-          </div>
-        </div>
-      );
+          </div> */}
+                    
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4">
+                            <NewsIteam title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                        </div>
+                    })}
+                </div>
+                {/* <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark left" onClick={this.handlePrevClick}>&larr; Left</button>
+            <button disable={this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark right" onClick={this.handleNextClick}>&rarr; Right</button>
+        
+          </div> */}
+                </InfiniteScroll>
+                {/* <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark left" onClick={this.handlePrevClick}>&larr; Left</button>
+            <button disable={this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark right" onClick={this.handleNextClick}>&rarr; Right</button>
+        
+          </div> */}
+            
+            </div>
+        );
     }
-  }
-  News.defaultProps= {
+}
+News.defaultProps = {
     country: 'in',
     pageSize: 8
-  }
-  News.propTypes={
+}
+News.propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number
-  }
+}
