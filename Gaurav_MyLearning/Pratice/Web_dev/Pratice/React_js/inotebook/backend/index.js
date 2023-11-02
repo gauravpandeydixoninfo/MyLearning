4997f32d98082567b7ec5ada771acb1041a453d3
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-11-02 15:52:10 +0530

    inotebook project started schema and express api done and data saved in database in mongo throught thunder-client

--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/inotebook/backend/index.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/inotebook/backend/index.js
@@ -1,14 +1,28 @@
 const db= require('./db');
 const express = require('express')
 
+
 // connectToMongo();
 
 db;
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hello gaurav')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('hello gaurav')
+// })
+
+// app.get('/api/vi/login', (req, res) => {
+//   res.send('hello login')
+// })
+
+// app.get('/api/vi/signup', (req, res) => {
+//   res.send('hello signup')
+// })
 
 app.listen(3001,()=>{
     console.log("app is listening at port 3000")
