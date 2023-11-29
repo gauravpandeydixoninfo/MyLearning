1c93ca1f982ebd0642fbf8c34182e38de8713518
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-11-29 11:52:49 +0530

    useContext hook working fine with useState, useEffect and setTimeout

---
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/inotebook/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      This is home
+    </div>
+  );
+}
+
+export default Home;
