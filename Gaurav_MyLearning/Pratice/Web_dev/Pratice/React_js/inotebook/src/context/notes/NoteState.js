e46766ef3ce8685f8f5cdc150d775d2e99ffac64
Gaurav Pandey <gaurav.pandey@dixoninfo.com>
2023-12-01 17:59:59 +0530

    updation of notes from backend and frontend

--- Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/inotebook/src/context/notes/NoteState.js
+++ Gaurav_MyLearning/Pratice/Web_dev/Pratice/React_js/inotebook/src/context/notes/NoteState.js
@@ -37,24 +37,25 @@ const NoteState = (props) => {
 
       body: JSON.stringify({title, description, tag})
     });
-    // const json = response.json();
-    console.log("adding a new note")
-    const note = {
-      "_id": "66758546f065d311009eb81fb",
-      "user": "65643c2afa3a71805a2e16df",
-      "title": "My title",
-      "description": "fgldy [ADDED]",
-      "tag": "personal",
-      "date": "2023-11-28T06:10:55.115Z",
-      "__v": 0
-    }
+    const note =await response.json();
+    
     setNotes(notes.concat(note));
 
   }
 
   //Delete a Note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
     //TODO: API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU2NDNjMmFmYTNhNzE4MDVhMmUxNmRmIn0sImlhdCI6MTcwMTA2NzgxOH0.NqgIg2MI-SaaCDCol2KH2BEQwEZR6b57ZjLz76DjrU4",
+      }
+   });
+    const json =  response.json();
+    console.log(json);
+
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
@@ -65,7 +66,7 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     //API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU2NDNjMmFmYTNhNzE4MDVhMmUxNmRmIn0sImlhdCI6MTcwMTA2NzgxOH0.NqgIg2MI-SaaCDCol2KH2BEQwEZR6b57ZjLz76DjrU4",
@@ -73,25 +74,28 @@ const NoteState = (props) => {
 
       body: JSON.stringify({title, description, tag})
     });
-    const json =  response.json();
+    const json =  await response.json();
+    console.log(json);
   
-
+  let newNotes = JSON.parse(JSON.stringify(notes))
 
   // Logic to edit in client
-  for (let index = 0; index < notes.length; index++) {
-    const element = notes[index];
+  for (let index = 0; index < newNotes.length; index++) {
+    const element = newNotes[index];
     if (element._id === id) {
-      element.title = title;
-      element.description = description;
-      element.tag = tag;
+      newNotes[index].title = title;
+      newNotes[index].description = description;
+      newNotes[index].tag = tag;
+      break;
     }
-
+    
   }
+  setNotes(newNotes)
 }
 
 
 return (
-  <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote, deleteNote, getNotes }}>
+  <NoteContext.Provider value={{ notes, setNotes, addNote,  editNote, deleteNote, getNotes }}>
     {props.children}
   </NoteContext.Provider>
 )
