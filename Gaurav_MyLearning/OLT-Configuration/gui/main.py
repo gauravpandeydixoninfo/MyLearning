8e2f323b94af442abc942ec927aeda3394cb2202
gauravpandeydixoninfo <gaurav.pandey@dixoninfo.com>
2025-03-21 10:47:00 +0530

    Remove the unwanted project files

--- Gaurav_MyLearning/OLT-Configuration/gui/main.py
+++
@@ -1,8 +0,0 @@
-from qtpy.QtWidgets import QApplication
-from gui.main_page import MainPage  # Use absolute import
-
-if __name__ == "__main__":
-    app = QApplication([])
-    window = MainPage()
-    window.show()
-    app.exec_()
